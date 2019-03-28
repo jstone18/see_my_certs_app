7398ae9fc0ece9520abe5ac1298c13d5055f6ced
jstone18 <jason.stone0808@gmail.com>
2019-03-28 03:00:58 -0400

    add #current_user and #logged_in? helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,16 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  helpers do
+
+    def current_user
+      User.find_by(id: session[:id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+  end
+
 end
