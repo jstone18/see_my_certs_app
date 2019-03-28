97bee130f4376517124fa6740cd41bdf70323d89
jstone18 <jason.stone0808@gmail.com>
2019-03-28 17:15:41 -0400

    create CertsController with RESTful route stubs, mount controller in config.ru

---
+++ app/controllers/certs_controller.rb
@@ -0,0 +1,37 @@
+class CertsController < ApplicationController
+
+  # GET: /certs
+  get "/certs" do
+    erb :"/certs/index.html"
+  end
+
+  # GET: /certs/new
+  get "/certs/new" do
+    erb :"/certs/new.html"
+  end
+
+  # POST: /certs
+  post "/certs" do
+    redirect "/certs"
+  end
+
+  # GET: /certs/5
+  get "/certs/:id" do
+    erb :"/certs/show.html"
+  end
+
+  # GET: /certs/5/edit
+  get "/certs/:id/edit" do
+    erb :"/certs/edit.html"
+  end
+
+  # PATCH: /certs/5
+  patch "/certs/:id" do
+    redirect "/certs/:id"
+  end
+
+  # DELETE: /certs/5/delete
+  delete "/certs/:id/delete" do
+    redirect "/certs"
+  end
+end
--- config.ru
+++ config.ru
@@ -7,4 +7,5 @@ end
 
 use Rack::MethodOverride
 run ApplicationController
+use CertsController
 use UsersController
