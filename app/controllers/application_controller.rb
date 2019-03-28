8e3fae253dbd7000f984268b2ab9c58aeae41645
jstone18 <jason.stone0808@gmail.com>
2019-03-27 20:21:22 -0400

    create route that renders index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
-require './config/environment'
-
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "ems2019"
   end
 
   get "/" do
-    erb :welcome
+    erb :index
   end
 
 end
