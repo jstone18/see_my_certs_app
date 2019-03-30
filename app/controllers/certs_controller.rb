e58db74f794cfdfc2b5d262ab568d2bfb9992746
jstone18 <jason.stone0808@gmail.com>
2019-03-29 21:29:33 -0400

    add functionality to edit and patch route, only a user logged in can edit a certification and a user can only edit their certifications

--- app/controllers/certs_controller.rb
+++ app/controllers/certs_controller.rb
@@ -30,25 +30,49 @@ class CertsController < ApplicationController
 
   # GET: /certs/5
   get "/certs/:id" do
-    @certs = Cert.find(params[:id])
+    set_cert
     erb :"/certs/show.html"
   end
 
   # GET: /certs/5/edit
   get "/certs/:id/edit" do
-    erb :"/certs/edit.html"
+    set_cert
+    if logged_in?
+      if @cert.user == current_user
+        erb :"/certs/edit.html"
+      else
+        redirect "/users/#{current_user.username}"
+      end
+    else
+      redirect "/"
+    end
   end
 
   # PATCH: /certs/5
   patch "/certs/:id" do
-    redirect "/certs/:id"
+    set_cert
+    if logged_in?
+      if @cert.user == current_user
+        @cert.update(cert_name: params[:cert_name], cert_number: params[:cert_number], exp_date: params[:exp_date])
+        redirect "/certs/#{@cert.id}"
+      else
+        redirect "/users/#{current_user.username}"
+      end
+    else
+      redirect "/"
+    end
   end
 
   # DELETE: /certs/5/delete
   delete "/certs/:id/delete" do
-    @cert = Cert.find(params[:id])
-    binding.pry
+    set_cert
     @cert.destroy
     redirect "/certs"
+  end
+
+  private
+
+  def set_cert
+    @cert = Cert.find(params[:id])
   end
 end
