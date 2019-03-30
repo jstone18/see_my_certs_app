4de0780ea78bdeb5c69843ded0c13b2464b8a339
jstone18 <jason.stone0808@gmail.com>
2019-03-30 14:59:31 -0400

    add validations for :full_name, :username, and :email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  validates :full_name, :username, :email, presence: true
+  validates :email, :username, uniqueness: true
 
   has_many :certs
 
